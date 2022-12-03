0c92602c049f09b52804e1434fec8f488d3566a0
Macx Buddhi Chaturanga <bupa8694@student.uu.se>
2022-12-03 13:56:41 +0530

    First Throw

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+template<typename T>
+auto foo(int x)-> decltype(x)
+{
+	return x*x;
+}
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+	int x = 4;
+	auto y = [&](){
+		return foo<int>(x);
+
+	};
+
+	std::cout << y ()<< std::endl;
 }
